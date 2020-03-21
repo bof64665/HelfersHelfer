9f9fd4bd5bea0d2e3cfe485a593e1dc175fa180d
antumin <m.vielberth@gmail.com>
2020-03-21 21:15:05 +0100

    add getHospitals and getHelper

--- src/app/services/firebase/firebase.service.ts
+++ src/app/services/firebase/firebase.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { first } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,32 @@ export class FirebaseService {
 
   constructor(private firestore: AngularFirestore) { }
 
-  public getKHList (){
-    this.firestore.collection('Intensivdb').valueChanges().subscribe(x =>{
-      console.log(x);
-    })
+  public async getHospitals(): Promise<Hospital[]> {
+    const hospitals: Observable<any[]> = this.firestore.collection('Intensivdb').valueChanges();
+    return hospitals.pipe(first()).toPromise() as Promise<Hospital[]>;
   }
+
+  public async getHelper(): Promise<Helper[]> {
+    const helpers: Observable<any[]> = this.firestore.collection('Helfer').valueChanges();
+    return helpers.pipe(first()).toPromise() as Promise<Helper[]>;
+  }
+}
+
+export interface Hospital {
+  address?: string;
+  city: string;
+  name: string;
+  name2?: string;
+  postcode?: string;
+  longitude?: string;
+  latitude?: string;
+}
+
+export interface Helper {
+  firstname: string;
+  lastname: string;
+  address: string;
+  verified: string;
+  helpWith: string[];
+  rating: string;
 }

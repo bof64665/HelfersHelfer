ead7cb0d025052e36b169668f7e33446e23d34c0
Fabian Böhm <fabian.boehm92@gmail.com>
2020-03-21 19:07:54 +0100

    Register Page MockUp

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  username: string;
+  pw: string;
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  onLogin() {
+    console.log(`user: ${this.username} | pw: ${this.pw}`);
+  }
 }

77fa7fbd74052163afe9a800b261681e3d32797f
antumin <m.vielberth@gmail.com>
2020-03-22 15:32:04 +0100

    add dummy data

--- src/app/pages/matching/components/map/map.component.ts
+++ src/app/pages/matching/components/map/map.component.ts
@@ -28,6 +28,7 @@ export class MapComponent implements OnInit {
   ngOnInit(): void {
     this.firebaseService.getHelper().then(helpers => {
       this.helpers = helpers;
+      console.log(helpers);
 
       this.firebaseService.getHospitals().then(hospitals => {
         this.hospitals = hospitals;
@@ -35,13 +36,18 @@ export class MapComponent implements OnInit {
         let layers = [];
 
         this.helpers.forEach(helper => {
-            layers.push(marker([ helper.latitude, helper.longitude ],
-              {icon: icon({
-                iconSize: [ 50, 40 ],
-                iconAnchor: [ 25, 25 ],
-                iconUrl: 'assets/helper.png',
-              })}
-              ).bindPopup(`<b>Helfer</b><br><br>${helper.firstname} ${helper.lastname}`).openPopup());
+          let helpertext = `<b>Helfer</b><br><br>${helper.firstname} ${helper.lastname}<br><br>Benötigt Hilfe bei:<br><ul>`;
+          helper.helpBy.forEach(help => {
+            helpertext += `<li>${help} <a href="">Hilfe anbieten</a></li>`;
+          });
+          helpertext += '</ul>';
+          layers.push(marker([ helper.latitude, helper.longitude ],
+            {icon: icon({
+              iconSize: [ 50, 40 ],
+              iconAnchor: [ 25, 25 ],
+              iconUrl: 'assets/helper.png',
+            })}
+            ).bindPopup(helpertext).openPopup());
         });
         this.hospitals.forEach(hospital => {
           layers.push(marker([ hospital.latitude, hospital.longitude ],
@@ -57,18 +63,18 @@ export class MapComponent implements OnInit {
                 <br/>
                 <br/>
                 <b>Spenden an das Personal: </b>
-  <br/>
-                 <br/>
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-<input type="hidden" name="cmd" value="_s-xclick" />
-<input type="hidden" name="hosted_button_id" value="B2ABTGYHD6K9S" />
-<input type="image" src="https://www.paypalobjects.com/en_US/DK/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-<img alt="" border="0" src="https://www.paypal.com/en_DE/i/scr/pixel.gif" width="1" height="1" />
-</form>
-<br/>
-<a href="https://www.bitcoinqrcodemaker.com" target="_blank">
-    <img src="https://www.bitcoinqrcodemaker.com/api/?style=bitcoincash&address=qpw5ptqvf3gutpskykvhwmnmtkgj3x8z2sk5ylqhv8" height="150" width="150" border="0" alt="Bitcoin Cash QR Code" />
-</a>
+                <br/>
+                <br/>
+                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                <input type="hidden" name="cmd" value="_s-xclick" />
+                <input type="hidden" name="hosted_button_id" value="B2ABTGYHD6K9S" />
+                <input type="image" src="https://www.paypalobjects.com/en_US/DK/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                <img alt="" border="0" src="https://www.paypal.com/en_DE/i/scr/pixel.gif" width="1" height="1" />
+                </form>
+                <br/>
+                <a href="https://www.bitcoinqrcodemaker.com" target="_blank">
+                    <img src="https://www.bitcoinqrcodemaker.com/api/?style=bitcoincash&address=qpw5ptqvf3gutpskykvhwmnmtkgj3x8z2sk5ylqhv8" height="150" width="150" border="0" alt="Bitcoin Cash QR Code" />
+                </a>
 
             `).openPopup());
         });
--- src/app/services/firebase/firebase.service.ts
+++ src/app/services/firebase/firebase.service.ts
@@ -7,17 +7,104 @@ import { first } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class FirebaseService {
+  helpers: Helper[] = [
+    {
+      firstname: 'Sepp',
+      lastname: 'Huber',
+      address: '',
+      verified: true,
+      helpBy: ['Einkäufe erledigen', 'Hund ausführen'],
+      rating: '',
+      latitude: 49.013748,
+      longitude: 12.083179
+    },
+    {
+      firstname: 'Anna',
+      lastname: 'Müller',
+      address: '',
+      verified: true,
+      helpBy: ['Einkäufe erledigen', 'Hund ausführen'],
+      rating: '',
+      latitude: 49.010376,
+      longitude: 12.105823
+    },
+    {
+      firstname: 'Angela',
+      lastname: 'Merkel',
+      address: '',
+      verified: true,
+      helpBy: ['Einkäufe erledigen', 'Hund ausführen'],
+      rating: '',
+      latitude: 49.018667,
+      longitude: 12.105687
+    },
+    {
+      firstname: 'Peter',
+      lastname: 'Söder',
+      address: '',
+      verified: true,
+      helpBy: ['Einkäufe erledigen', 'Hund ausführen'],
+      rating: '',
+      latitude: 49.022297,
+      longitude: 12.086740
+    },
+    {
+      firstname: 'Sepp',
+      lastname: 'Huber',
+      address: '',
+      verified: true,
+      helpBy: ['Einkäufe erledigen', 'Hund ausführen'],
+      rating: '',
+      latitude: 49.020486,
+      longitude: 12.058422
+    },
+  ];
+
+  hospitals: Hospital[] =[
+    {
+      name: "Universitätsklinikum Regensburg",
+      address: "Franz-Josef-Strauss Allee 11",
+      postcode:"93053",
+      city:"Regensburg",
+      longitude:12.089136,
+      latitude:48.987642
+    },
+    {
+      name:"Caritas Krankenhaus St. Josef",
+      name2:"Anaesthesiologie und Intensivmedizin",
+      address:"Landshuter Str. 65",
+      postcode:"93053",
+      city:"Regensburg",
+      longitude:12.117537,
+      latitude:49.007758
+    },
+    {
+      name:"Krankenhaus Barmherzige Brüder Regensburg",
+      name2:"Pneumologie und konservative Intensivmedizin",
+      address:"Prüfeninger Straße 86",
+      postcode:"93049",
+      city:"Regensburg",
+      longitude:12.07968,
+      latitude:49.05215
+    }
+    ];
 
   constructor(private firestore: AngularFirestore) { }
 
   public async getHospitals(): Promise<Hospital[]> {
-    const hospitals: Observable<any[]> = this.firestore.collection('Intensivdb').valueChanges();
-    return hospitals.pipe(first()).toPromise() as Promise<Hospital[]>;
+    // const hospitals: Observable<any[]> = this.firestore.collection('Intensivdb').valueChanges();
+    // return hospitals.pipe(first()).toPromise() as Promise<Hospital[]>;
+    return new Promise((resolve, reject)=>{
+      resolve(this.hospitals)
+    })
   }
 
   public async getHelper(): Promise<Helper[]> {
-    const helpers: Observable<any[]> = this.firestore.collection('Helfer').valueChanges();
-    return helpers.pipe(first()).toPromise() as Promise<Helper[]>;
+    // const helpers: Observable<any[]> = this.firestore.collection('Helfer').valueChanges();
+    // return helpers.pipe(first()).toPromise() as Promise<Helper[]>;
+    return new Promise((resolve, reject)=>{
+      resolve(this.helpers)
+    })
   }
 
   public async getHelperHelper(): Promise<HelperHelper[]> {
@@ -40,7 +127,7 @@ export interface Helper {
   firstname: string;
   lastname: string;
   address: string;
-  verified: string;
+  verified: boolean;
   helpBy: string[];
   rating: string;
   longitude: number;

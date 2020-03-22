1651a756dd4f835a6a0b798d742c853d34bbc75d
antumin <m.vielberth@gmail.com>
2020-03-22 12:05:12 +0100

    add custom markers

--- src/app/pages/matching/components/map/map.component.ts
+++ src/app/pages/matching/components/map/map.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { tileLayer, latLng, circle, polygon, marker } from 'leaflet';
-import { Hospital, FirebaseService } from 'src/app/services/firebase/firebase.service';
+import { tileLayer, latLng, circle, polygon, marker, icon } from 'leaflet';
+import { Hospital, FirebaseService, Helper, HelperHelper } from 'src/app/services/firebase/firebase.service';
 
 @Component({
   selector: 'app-map',
@@ -8,15 +8,16 @@ import { Hospital, FirebaseService } from 'src/app/services/firebase/firebase.se
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
-  private hospitals: Hospital[];
+  private helpers: Helper[];
+  private helperhelpers: HelperHelper[];
 
   options = {
     layers: [
       tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       { maxZoom: 18, attribution: '...' })
     ],
-    zoom: 5,
-    center: latLng(51.163361111111, 10.447683333333)
+    zoom: 13,
+    center: latLng(49.016196, 12.097291)
   };
 
   layers = [
@@ -25,14 +26,29 @@ export class MapComponent implements OnInit {
   constructor(public firebaseService: FirebaseService) { }
 
   ngOnInit(): void {
-    this.firebaseService.getHospitals().then(hospitals => {
-      this.hospitals = hospitals;
+    this.firebaseService.getHelper().then(helpers => {
+      this.helpers = helpers;
 
-      let layers = [];
-      this.hospitals.forEach(hospital => {
-          layers.push(marker([ hospital.latitude, hospital.longitude ]).bindPopup(`Name: ${hospital.name}<br> City: ${hospital.city}`).openPopup());
+      this.firebaseService.getHelperHelper().then(helperhelpers => {
+        this.helperhelpers = helperhelpers;
+
+        let layers = [];
+
+        this.helpers.forEach(helper => {
+            layers.push(marker([ helper.latitude, helper.longitude ],
+              {icon: icon({
+                iconSize: [ 25, 41 ],
+                iconAnchor: [ 13, 41 ],
+                iconUrl: 'assets/uni.png',
+              })}
+              ).bindPopup(`<b>Helfer</b><br><br>Name: ${helper.firstname} ${helper.lastname}`).openPopup());
+        });
+        this.helperhelpers.forEach(helperhelper => {
+          layers.push(marker([ helperhelper.latitude, helperhelper.longitude ]).bindPopup(`<b>HelferHelfer</b><br><br>Name: ${helperhelper.firstname} ${helperhelper.lastname}`).openPopup());
+        });
+
+        this.layers = layers;
       });
-      this.layers = layers;
     });
   }
 
